043b867251732ba91f736f7b0e4ce61da2fdcae5
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-06-03 09:11:25 -0300

    Implementar botão para enviar numero

--- Aplicativos/Tabuada/app/src/main/java/br/com/zup/tabuada/MainActivity.kt
+++ Aplicativos/Tabuada/app/src/main/java/br/com/zup/tabuada/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.zup.tabuada
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.zup.tabuada.databinding.ActivityMainBinding
@@ -7,15 +8,34 @@ import br.com.zup.tabuada.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var numeroDigitado: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener {
+            enviarNumero()
+        }
     }
 
-//    TODO criar fun para recuperar numero digitado
-//    TODO criar fun para enviar numero
+    private fun enviarNumero(){
+        recuperarNumerodigitado()
+        val intent = Intent(this, TelaDeResultado::class.java).apply {
+            putExtra("Numero",numeroDigitado)
+        }
+        startActivity(intent)
+    }
+
+    private fun recuperarNumerodigitado(){
+        this.numeroDigitado = binding.etValorTabuada.text.toString()
+    }
+
+
+
+// ok   TODO criar fun para recuperar numero digitado
+// ok   TODO criar fun para enviar numero
 //    TODO criar validação de dados
 //    TODO limpar dados
 }
